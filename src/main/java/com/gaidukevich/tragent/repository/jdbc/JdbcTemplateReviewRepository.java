0e33e202b99fb9b535c181249b245775a1443b3f
Sergey Gaidukevich <cergey231@tut.by>
2019-02-19 20:59:44 +0300

    add constructor with jdbcTem param

--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateReviewRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateReviewRepository.java
@@ -1,4 +1,11 @@
 package com.gaidukevich.tragent.repository.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class JdbcTemplateReviewRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateReviewRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
