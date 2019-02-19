2b84f5b7841eee7f831b4ec6082860a4aad60114
Sergey Gaidukevich <cergey231@tut.by>
2019-02-19 21:01:26 +0300

    refactor select country

--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateHotelRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateHotelRepository.java
@@ -5,7 +5,6 @@ import com.gaidukevich.tragent.entity.Hotel;
 import com.gaidukevich.tragent.repository.HotelRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -15,25 +14,27 @@ import java.util.Map;
 public class JdbcTemplateHotelRepository implements HotelRepository {
     private static final String SQL_INSERT_HOTEL = "INSERT INTO hotels (name, phone, country_id, stars)" +
             " VALUES (:name, :phone, :country_id, :stars)";
-    private static final String SQL_DELETE_HOTEL_BY_ID = "DELETE FROM hotels WHERE hotel_id = ";
+    private static final String SQL_DELETE_HOTEL_BY_ID = "DELETE FROM hotels WHERE hotel_id = ?";
     private static final String SQL_UPDATE_HOTEL_BY_ID = "UPDATE hotels SET name = :name, phone = :phone," +
             " country_id = :country, stars = :stars WHERE hotel_id = :id";
-    private static final String SQL_SELECT_ALL_HOTELS = "SELECT * from hotels";
+    private static final String SQL_SELECT_ALL_HOTELS = "SELECT * FROM hotels LEFT JOIN country" +
+            " ON hotels.country_id = countries.country_id";
     private static final String SQL_UPDATE_HOTELS_NAME_BY_ID = "UPDATE hotels SET name = :name WHERE hotel_id = :id";
     private static final String SQL_UPDATE_HOTELS_PHONE_BY_ID = "UPDATE hotels SET phone = :phone WHERE hotel_id = :id";
-    private static final String SQL_UPDATE_HOTELS_COUNTRY_ID_BY_HOTEL_ID = "UPDATE hotels SET country_id = :country_id WHERE hotel_id = :id";
+    private static final String SQL_UPDATE_HOTELS_COUNTRY_ID_BY_HOTEL_ID = "UPDATE hotels SET country_id = :country_id" +
+            " WHERE hotel_id = :id";
     private static final String SQL_UPDATE_HOTELS_STARS_BY_ID = "UPDATE hotels SET stars = :stars WHERE hotel_id = :id";
-    private static final String SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID = "SELECT * from countries WHERE country_id = :id";
+
     private JdbcTemplate jdbcTemplate;
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public JdbcTemplateHotelRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void add(Hotel hotel) {
         Map<String, Object> params = new HashMap<>();
-        putSomeHotelParametersInMap(params, hotel);
+        putHotelParametersInMap(params, hotel);
 
         jdbcTemplate.update(SQL_INSERT_HOTEL, params);
     }
@@ -47,7 +48,7 @@ public class JdbcTemplateHotelRepository implements HotelRepository {
     public void update(Long id, Hotel hotel) {
         Map<String, Object> params = new HashMap<>();
         params.put("id", id);
-        putSomeHotelParametersInMap(params, hotel);
+        putHotelParametersInMap(params, hotel);
 
         jdbcTemplate.update(SQL_UPDATE_HOTEL_BY_ID, params);
     }
@@ -99,7 +100,7 @@ public class JdbcTemplateHotelRepository implements HotelRepository {
         return null;
     }
 
-    private void putSomeHotelParametersInMap(Map<String, Object> params, Hotel hotel) {
+    private void putHotelParametersInMap(Map<String, Object> params, Hotel hotel) {
         params.put("name", hotel.getName());
         params.put("phone", hotel.getPhone());
         params.put("country_id", hotel.getCountry().getId());
@@ -113,19 +114,11 @@ public class JdbcTemplateHotelRepository implements HotelRepository {
         hotel.setPhone(rs.getString("phone"));
 
         Long country_id = rs.getLong("country_id");
-        Country country = jdbcTemplate.queryForObject(SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID, this::mapCountry, country_id);
-        hotel.setCountry(country);
+        String country_name = rs.getString("country_name");
+        hotel.setCountry(new Country(country_id, country_name));
 
         hotel.setStars(rs.getInt("stars"));
 
         return hotel;
-    }
-
-    private Country mapCountry(ResultSet rs, int rowNum) throws SQLException {
-        Country country = new Country();
-        country.setId(rs.getLong("country_id"));
-        country.setName(rs.getString("name"));
-
-        return country;
     }
 }
