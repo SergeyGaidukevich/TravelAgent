dfcd23797555188b7f1461f5f45b9bcd5a67ea7d
Sergey Gaidukevich <cergey231@tut.by>
2019-02-19 20:59:44 +0300

    add constructor with jdbcTem param

--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateCountryRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateCountryRepository.java
@@ -1,4 +1,23 @@
 package com.gaidukevich.tragent.repository.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class JdbcTemplateCountryRepository {
+    private static final String SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID = "SELECT * from countries WHERE country_id = :id";
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateCountryRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Country country = jdbcTemplate.queryForObject(SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID, this::mapCountry, country_id);
+//
+//    private Country mapCountry(ResultSet rs, int rowNum) throws SQLException {
+//        Country country = new Country();
+//        country.setId(rs.getLong("country_id"));
+//        country.setName(rs.getString("name"));
+//
+//        return country;
+//}
 }
--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateTourRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateTourRepository.java
@@ -1,4 +1,11 @@
 package com.gaidukevich.tragent.repository.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class JdbcTemplateTourRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateTourRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
