75b7b9ed2c7c9f18fc22fdfe77472fcd7bbae36e
Sergey Gaidukevich <cergey231@tut.by>
2019-02-25 11:56:00 +0300

    implements jdbc

--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateCountryRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateCountryRepository.java
@@ -1,9 +1,23 @@
 package com.gaidukevich.tragent.repository.jdbc;
 
+import com.gaidukevich.tragent.entity.Country;
+import com.gaidukevich.tragent.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class JdbcTemplateCountryRepository {
-    private static final String SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID = "SELECT * from countries WHERE country_id = :id";
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JdbcTemplateCountryRepository implements Repository<Country> {
+    private static final String SQL_SELECT_COUNTRY_BY_ID = "SELECT * FROM countries WHERE country_id = ?";
+    private static final String SQL_INSERT_COUNTRY = "INSERT INTO countries (country_name) VALUES (?)";
+    private static final String SQL_DELETE_COUNTRY_BY_ID = "DELETE FROM countries WHERE country_id = ?";
+    private static final String SQL_SELECT_ALL_COUNTRIES = "SELECT * FROM countries";
+    private static final String SQL_UPDATE_COUNTRY_BY_ID = "UPDATE countries SET country_name = :country_name" +
+            " WHERE country_id = :country_id";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -11,13 +25,40 @@ public class JdbcTemplateCountryRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    // Country country = jdbcTemplate.queryForObject(SQL_ELECT_ALL_FROM_COUNTRIES_BY_ID, this::mapCountry, country_id);
-//
-//    private Country mapCountry(ResultSet rs, int rowNum) throws SQLException {
-//        Country country = new Country();
-//        country.setId(rs.getLong("country_id"));
-//        country.setName(rs.getString("name"));
-//
-//        return country;
-//}
+    @Override
+    public void add(Country country) {
+        jdbcTemplate.update(SQL_INSERT_COUNTRY, country.getName());
+    }
+
+    @Override
+    public void remove(Long id) {
+        jdbcTemplate.update(SQL_DELETE_COUNTRY_BY_ID, id.intValue());
+    }
+
+    @Override
+    public void update(Long id, Country country) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("country_id", country.getId());
+        params.put("country_name", country.getName());
+
+        jdbcTemplate.update(SQL_UPDATE_COUNTRY_BY_ID, params);
+    }
+
+    @Override
+    public List<Country> getAll() {
+        return jdbcTemplate.query(SQL_SELECT_ALL_COUNTRIES, this::mapCountry);
+    }
+
+    @Override
+    public Country getById(Long id) {
+        return jdbcTemplate.queryForObject(SQL_SELECT_COUNTRY_BY_ID, this::mapCountry, id.intValue());
+    }
+
+    private Country mapCountry(ResultSet rs, int rowNum) throws SQLException {
+        Country country = new Country();
+        country.setId(rs.getLong("country_id"));
+        country.setName(rs.getString("country_name"));
+
+        return country;
+}
 }
--- src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateUserRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/jdbc/JdbcTemplateUserRepository.java
@@ -1,13 +1,36 @@
 package com.gaidukevich.tragent.repository.jdbc;
 
+import com.gaidukevich.tragent.entity.Review;
 import com.gaidukevich.tragent.entity.Tour;
 import com.gaidukevich.tragent.entity.User;
-import com.gaidukevich.tragent.repository.UserRepository;
+import com.gaidukevich.tragent.repository.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
-public class JdbcTemplateUserRepository implements UserRepository {
+public class JdbcTemplateUserRepository implements Repository<User> {
+    private static final String SQL_SELECT_ALL_USERS = "SELECT users.user_id, users.login, users.password," +
+            " user_tours.tour_id, reviews.review_id FROM users" +
+            " LEFT JOIN reviews ON reviews.user_id = users.user_id" +
+            " LEFT JOIN user_tours ON users.user_id = user_tours.user_id";
+    private static final String SQL_SELECT_USER_BY_ID = "SELECT users.user_id, users.login, users.password," +
+            " user_tours.tour_id, reviews.review_id FROM users" +
+            " LEFT JOIN reviews ON reviews.user_id = users.user_id" +
+            " LEFT JOIN user_tours ON users.user_id = user_tours.user_id  WHERE users.user_id = :user_id";
+    private static final String SQL_INSERT_USER = "INSERT INTO travel_agent.users (login, password)" +
+            " VALUES (:login, :password)";
+    private static final String SQL_DELETE_USER_BY_ID = "DELETE FROM users WHERE user_id = ?";
+    private static final String UPDATE_USER_BY_ID = "UPDATE users SET login = :login, password = :password WHERE user_id = :user_id";
+
     private JdbcTemplate jdbcTemplate;
 
     public JdbcTemplateUserRepository(JdbcTemplate jdbcTemplate) {
@@ -15,67 +38,92 @@ public class JdbcTemplateUserRepository implements UserRepository {
     }
 
     @Override
-    public void updateLogin(Long id, String login) {
-
-    }
-
-    @Override
-    public void updatePassword(Long id, String password) {
-
-    }
-
-    @Override
-    public void updateTours(Long id, List<Tour> tours) {
-
-    }
-
-    @Override
-    public void updateReview(Long id, List<Tour> reviews) {
-
-    }
-
-    @Override
     public void add(User user) {
-//        String sqlAddUser = "INSERT INTO travel_agent.users (login, password) VALUES (?, ?);";
-//        jdbcTemplate.update(sqlAddUser, user.getLogin(), user.getPassword());
-//
-//        List<Review> reviews = user.getReviews();
-//        String sqlAddReviews = "INSERT INTO travel_agent.reviews (tour_id, user_id, content)" +
-//                " VALUES (?, ?, ?);";
-//        reviews.forEach(review -> jdbcTemplate.update(sqlAddReviews, review.getTour(), review.getUser(),
-//                review.getContent()));
-//
-//
-//        List<Tour> tours = user.getTours();
-//        String sqlAddTours = "INSERT INTO travel_agent.tours (photo, date, duration, country_id, hotel_id, type, description, cost)" +
-//                " VALUES ('?', '?', '?', '?', '?', '?', '?', '?');";
-//        tours.forEach(tour -> jdbcTemplate.update(sqlAddTours, tour.getPhoto(), tour.getDate(), tour.getDuration(),
-//                 tour.getCountry(), tour.getHotel(), tour.getType(), tour.getDescription(), tour.getCost()));
-//
-//        String sqlAddUserTours = "INSERT INTO travel_agent.user_tours (tour_id, user_id) " +
-//                " VALUES (?, ?);";
-//        tours.forEach(tour -> jdbcTemplate.update(sqlAddUserTours));
+        Map<String, Object> params = new HashMap<>();
+        putUserParametersInMap(params, user);
 
-
+        jdbcTemplate.update(SQL_INSERT_USER, params);
     }
 
     @Override
     public void remove(Long id) {
-
+        jdbcTemplate.update(SQL_DELETE_USER_BY_ID, id.intValue());
     }
 
     @Override
-    public void update(Long id, User entity) {
+    public void update(Long id, User user) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("user_id", user.getId());
+        putUserParametersInMap(params, user);
 
+        jdbcTemplate.update(UPDATE_USER_BY_ID, params);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return jdbcTemplate.query(SQL_SELECT_ALL_USERS, new UserMapExtractor());
     }
 
     @Override
     public User getById(Long id) {
-        return null;
+        return Objects.requireNonNull(jdbcTemplate.query(SQL_SELECT_USER_BY_ID, new UserMapExtractor(),
+                id.intValue())).get(0);
+    }
+
+    private void putUserParametersInMap(Map<String, Object> params, User user) {
+        params.put("login", user.getLogin());
+        params.put("password", user.getPassword());
+    }
+
+    private static final class UserMapExtractor implements ResultSetExtractor<List<User>> {
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException {
+            Map<Long, User> users = new HashMap<>();
+            Map<Long, Set<Tour>> userTourMap = new HashMap<>();
+            Map<Long, Set<Review>> userReviewMap = new HashMap<>();
+
+            while (rs.next()) {
+                Long user_id = (long) rs.getInt("user_id");
+                String login = rs.getString("login");
+                String password = rs.getString("password");
+                Long tour_id = (long) rs.getInt("tour_id");
+                Long review_id = (long) rs.getInt("review_id");
+
+                Set<Tour> tours = userTourMap.get(user_id);
+                Tour tour = new Tour();
+                tour.setId(tour_id);
+                if (tours == null) {
+                    tours = new HashSet<>();
+                    tours.add(tour);
+                    userTourMap.put(user_id, tours);
+                } else {
+                    tours.add(tour);
+                }
+
+                Set<Review> reviews = userReviewMap.get(user_id);
+                Review review = new Review();
+                review.setId(review_id);
+                if (reviews == null) {
+                    reviews = new HashSet<>();
+                    reviews.add(review);
+                    userReviewMap.put(user_id, reviews);
+                } else {
+                    reviews.add(review);
+                }
+
+                User user = users.get(user_id);
+                if (user == null) {
+                    List<Tour> toursList = new ArrayList<>(userTourMap.get(user_id));
+                    List<Review> reviewList = new ArrayList<>(userReviewMap.get(user_id));
+                    user = new User(user_id, login, password, toursList, reviewList);
+                    users.put(user_id, user);
+                } else {
+                    user.getTours().add(tour);
+                    user.getReviews().add(review);
+                }
+            }
+
+            return new ArrayList<>(users.values());
+        }
     }
 }
