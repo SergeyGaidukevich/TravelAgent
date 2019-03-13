2a61ca4494cbf97655ff2ae4dd0518a82d42e05c
Sergey Gaidukevich <cergey231@tut.by>
2019-03-13 17:43:45 +0300

    renamed from Repository to EntityRepository

--- src/main/java/com/gaidukevich/tragent/repository/HotelRepository.java
+++ src/main/java/com/gaidukevich/tragent/repository/HotelRepository.java
@@ -3,7 +3,7 @@ package com.gaidukevich.tragent.repository;
 import com.gaidukevich.tragent.entity.Country;
 import com.gaidukevich.tragent.entity.Hotel;
 
-public interface HotelRepository extends Repository<Hotel> {
+public interface HotelRepository extends EntityRepository<Hotel> {
     void updateName(Long id, String name);
 
     void updatePhone(Long id, String phone);
