360e0cdb77f9329394812a1a954e0c7a1aa05636
Sergey Gaidukevich <cergey231@tut.by>
2019-03-18 14:53:13 +0300

    refactor property and add aspect

--- src/main/java/com/gaidukevich/tragent/configuration/ConfigurationBeans.java
+++ src/main/java/com/gaidukevich/tragent/configuration/ConfigurationBeans.java
@@ -1,12 +1,13 @@
 package com.gaidukevich.tragent.configuration;
 
 import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -15,38 +16,45 @@ import org.springframework.transaction.support.TransactionTemplate;
 @Configuration
 @PropertySource("classpath:database.properties")
 @ComponentScan("com.gaidukevich.tragent")
+@EnableAspectJAutoProxy
 public class ConfigurationBeans {
-    private static final String DB_DRIVER = "db.driver";
-    private static final String DB_USER = "db.user";
-    private static final String DB_PASSWORD = "db.password";
-    private static final String DB_URL = "db.url";
+    @Value("${db.driver}")
+    private String dbDriver;
 
-    private final Environment environment;
+    @Value("${db.url}")
+    private String dbUrl;
 
-    @Autowired
-    public ConfigurationBeans(Environment environment) {
-        this.environment = environment;
+    @Value("${db.user}")
+    private String dbUserName;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${db.minConnection}")
+    private String dbMinConnection;
+
+    @Value("${db.maxConnection}")
+    private String dbMaxConnection;
+
+    @Value("${db.maxOpenPreparedStatements}")
+    private String dbMaxOpenPreparedStatements;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Bean
     public BasicDataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        String driver = environment.getProperty(DB_DRIVER);
-        String url = environment.getProperty(DB_URL);
-        String userName = environment.getProperty(DB_USER);
-        String password = environment.getProperty(DB_PASSWORD);
 
-        if (driver != null && url != null && userName != null && password != null) {
-            dataSource.setDriverClassName(driver);
-            dataSource.setUrl(url);
-            dataSource.setUsername(userName);
-            dataSource.setPassword(password);
-            dataSource.setMinIdle(5);
-            dataSource.setMaxIdle(10);
-            dataSource.setMaxOpenPreparedStatements(50);
-        } else {
-            //log
-        }
+        dataSource.setDriverClassName(dbDriver);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUserName);
+        dataSource.setPassword(dbPassword);
+        dataSource.setMinIdle(Integer.parseInt(dbMinConnection));
+        dataSource.setMaxIdle(Integer.parseInt(dbMaxConnection));
+        dataSource.setMaxOpenPreparedStatements(Integer.parseInt(dbMaxOpenPreparedStatements));
 
         return dataSource;
     }
